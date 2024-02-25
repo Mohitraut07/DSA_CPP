78bee0302f9922750287a12b3c233837856518c9
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2024-02-25 16:28:49 +0530

    Fix formatting and update map declaration syntax

--- stlMap.cpp
+++ stlMap.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 #include <map>
 using namespace std;
 
@@ -6,7 +6,7 @@ int main()
 {
     map<int, int> mpp;
 
-    map<int, pair<int, int> > mpp1;
+    map<int, pair<int, int>> mpp1;
 
     map<pair<int, int>, int> mpp2;
 
@@ -15,22 +15,34 @@ int main()
     // mpp2[{2, 3}] = 10;
 
     // Output the content of mpp
-    for (auto it : mpp) {
+    for (auto it : mpp)
+    {
         cout << it.first << " " << it.second << endl;
     }
 
     // Accessing elements of mpp
     cout << "Value of mpp[1]: " << mpp[1] << endl;
-    
+
     // Attempting to access a key that does not exist in mpp
     // cout << "Value of mpp[5]: " << mpp[5] << endl; // This will cause an error since mpp[5] is not initialized
 
     // You can check if a key exists in the map before accessing it
-    if (mpp.find(5) != mpp.end()) {
+    if (mpp.find(5) != mpp.end())
+    {
         cout << "Value of mpp[5]: " << mpp[5] << endl;
-    } else {
+    }
+    else
+    {
         cout << "Key 5 does not exist in mpp" << endl;
     }
 
+    auto it = mpp.find(3);
+    // cout << *(it) << endl;
+
+    auto it1 = mpp.lower_bound(2);
+
+    auto it2 = mpp.upper_bound(5);
+    
+    
     return 0;
 }
