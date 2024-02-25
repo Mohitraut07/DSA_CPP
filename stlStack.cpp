9bed608136333213b2681815b316829a57ab6a83
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2024-02-25 12:00:30 +0530

    Add stlStack.cpp with stack operations

---
+++ stlStack.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include <stack>
+using namespace std;
+
+int main(){
+    stack<int> s;
+    s.push(0);
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    // s.emplace(5);
+
+    cout << s.top() << ' ';//4
+
+    cout << s.size() << " ";
+
+    cout << s.empty() << " ";
+    // cout << s.top();
+    stack<int> copy;
+    copy.swap(s);
+
+    //! Note: emplace()  access methods does not work in new version
+    return 0;
+}
