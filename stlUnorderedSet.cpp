b0dcdbdee2885722e78796c838b06a90b048a685
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2024-02-25 15:40:51 +0530

    Add stlUnorderedSet.cpp file with unordered_set implementation

---
+++ stlUnorderedSet.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <unordered_set> // Include the <unordered_set> header file
+
+using namespace std;
+
+int main()
+{
+    unordered_set<int> st;
+    /*
+        lower_bound and upper_bound function
+        does not work, rest all functions are same
+        as above, it does stores in any
+        particular order it has a better complexity
+        than set in most cases, except some when collision happens
+    */
+
+    return 0;
+}
